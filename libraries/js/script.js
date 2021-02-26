ef7be5508569475f97ad8c9146ddb3ff04e7a2ca
ilhamnopihendri <ilhamnopihendri@gmail.com>
2021-02-26 19:35:54 +0700

    menambahkan dan merubah isi form register show password

--- libraries/js/script.js
+++ libraries/js/script.js
@@ -1,58 +1,59 @@
 // DATA-AOS
 
-  AOS.init();
+AOS.init();
 
 // JS DOCUMENT
-$(document).ready(function() {
- $('#autoWidth').lightSlider({
-     autoWidth:true,
-     loop:true,
-     onSliderLoad: function() {
-         $('#autoWidth').removeClass('cS-hidden');
-     } 
- });  
+$(document).ready(function () {
+  $("#autoWidth").lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function () {
+      $("#autoWidth").removeClass("cS-hidden");
+    },
+  });
 });
 
 // smooth scrool
-$(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-     
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
           // Add hash (#) to URL when done scrolling (default click behavior)
           window.location.hash = hash;
-        });
-      } // End if
-    });
+        }
+      );
+    } // End if
   });
+});
 
 //   BUTTON SCROLL TOP
 
-jQuery(document).ready(function($){
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 50) {
-            $('#backToTop').fadeIn('slow');
-        } else {
-            $('#backToTop').fadeOut('slow');
-        }
-    });
-    $('#backToTop').click(function(){
-        $("html, body").animate({ scrollTop: 0 }, 500);
-        return false;
-    });
+jQuery(document).ready(function ($) {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $("#backToTop").fadeIn("slow");
+    } else {
+      $("#backToTop").fadeOut("slow");
+    }
+  });
+  $("#backToTop").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 500);
+    return false;
+  });
 });
-
 
