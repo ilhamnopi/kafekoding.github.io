c3267da1edfba3ea19ec167ca9bd6bb20a25bd81
ilhamnopihendri <ilhamnopihendri@gmail.com>
2021-03-08 02:53:16 +0700

    Menambah enscrypt

--- libraries/js/showPassword.js
+++ libraries/js/showPassword.js
@@ -1,13 +1,13 @@
- // Show hide Password
- const password = document.getElementById('password');
- const button_show_hide = document.getElementById('button_show_hide');
- 
- function showHide() {
-   if (password.type === 'password') {
-     password.setAttribute('type', 'text');
-     button_show_hide.classList.add('hide');
-   } else {
-     password.setAttribute('type', 'password');
-     button_show_hide.classList.remove('hide');
-   }
- }
+// Show hide Password
+const password = document.getElementById("password");
+const button_show_hide = document.getElementById("button_show_hide");
+
+function showHide() {
+  if (password.type === "password") {
+    password.setAttribute("type", "text");
+    button_show_hide.classList.add("hide");
+  } else {
+    password.setAttribute("type", "password");
+    button_show_hide.classList.remove("hide");
+  }
+}
